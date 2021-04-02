9b147d831f3adb86d1a3484be5aacd4462e7b107
mrodriguezdelcorral <mariavictoriavrod@lewisu.edu>
2021-04-02 10:55:21 -0500

    updated mosaic 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Intermediate from './Intermediate';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Intermediate />, 
+	document.getElementById('root')
+);
